e87f5fc4962b5d4bfde79eba2638a77c31cb128b
hua <924193579@qq.com>
2019-05-22 18:39:49 +0800

    第一个版本的另一个思路

--- main.js
+++ main.js
@@ -1,47 +1,42 @@
+$('.images img:nth-child(1)').addClass('current')
+$('.images img:nth-child(2)').addClass('enter')
+$('.images img:nth-child(3)').addClass('enter')
 setTimeout(function(){
-    $('.images img:nth-child(1)').css({'transform': 'translateX(-100%)'})
-    $('.images img:nth-child(2)').css({'transform': 'translateX(-100%)'})
-    $('.images img:nth-child(1)').one('transitionend',function(){ //用.on的意思是只要transitionend之后都会执行，而.one是transitionend之后只执行一次
-        $('.images img:nth-child(1)').css({'transform': 'none'})
-        .addClass('right')
+    $('.images img:nth-child(1)').removeClass('current enter').addClass('leave')
+    $('.images img:nth-child(2)').removeClass('enter right').addClass('current')
+    $('.images img:nth-child(1)').one('transitionend',function(x){
+        $('.images img:nth-child(1)').removeClass('leave').addClass('right')
     })
-},2000)
+},3000)
+
 setTimeout(function(){
-    $('.images img:nth-child(2)').css({'transform': 'translateX(-200%)'})
-    $('.images img:nth-child(3)').css({'transform': 'translateX(-100%)'})
-    $('.images img:nth-child(2)').one('transitionend',function(){
-        $('.images img:nth-child(2)').css({'transform': 'none'})
-        .addClass('right')
-    })
-},4000)
-setTimeout(function(){
-    $('.images img:nth-child(3)').css({'transform': 'translateX(-200%)'})
-    $('.images img:nth-child(1)').css({'transform': 'translateX(-100%)','z-index':'1'})
-    $('.images img:nth-child(3)').one('transitionend',function(){
-        $('.images img:nth-child(3)').css({'transform': 'none'})
-        .addClass('right')
+    $('.images img:nth-child(2)').removeClass('current enter').addClass('leave')
+    $('.images img:nth-child(3)').removeClass('enter right').addClass('current')
+    $('.images img:nth-child(2)').one('transitionend',function(x){
+        $('.images img:nth-child(2)').removeClass('leave').addClass('right')
     })
 },6000)
-setTimeout(function(){
-    $('.images img:nth-child(1)').css({'transform': 'translateX(-200%)'})
-    $('.images img:nth-child(2)').css({'transform': 'translateX(-100%)','z-index':'1'})
-    $('.images img:nth-child(1)').one('transitionend',function(){
-        $('.images img:nth-child(1)').css({'transform': 'none'})
-        .addClass('right')
-    })
-},8000)
-setTimeout(function(){
-    $('.images img:nth-child(2)').css({'transform': 'translateX(-200%)'})
-    $('.images img:nth-child(3)').css({'transform': 'translateX(-100%)','z-index':'1'})
-    $('.images img:nth-child(2)').one('transitionend',function(){
-        $('.images img:nth-child(2)').css({'transform': 'none'})
-        .addClass('right')
-    })
-},10000)
-setTimeout(function(){
-    $('.images img:nth-child(3)').css({'transform': 'translateX(-200%)'})
-    $('.images img:nth-child(1)').css({'transform': 'translateX(-100%)','z-index':'1'})
-    $('.images img:nth-child(1)').one('transitionend',function(){
 
+setTimeout(function(){
+    $('.images img:nth-child(3)').removeClass('current enter').addClass('leave')
+    $('.images img:nth-child(1)').removeClass('enter right').addClass('current')
+    $('.images img:nth-child(3)').one('transitionend',function(x){
+        $('.images img:nth-child(3)').removeClass('leave').addClass('right')
+    })
+},9000)
+
+setTimeout(function(){
+    $('.images img:nth-child(1)').removeClass('current enter').addClass('leave')
+    $('.images img:nth-child(2)').removeClass('enter right').addClass('current')
+    $('.images img:nth-child(1)').one('transitionend',function(x){
+        $('.images img:nth-child(1)').removeClass('leave').addClass('right')
     })
 },12000)
+
+setTimeout(function(){
+    $('.images img:nth-child(2)').removeClass('current enter').addClass('leave')
+    $('.images img:nth-child(3)').removeClass('enter right').addClass('current')
+    $('.images img:nth-child(2)').one('transitionend',function(x){
+        $('.images img:nth-child(2)').removeClass('leave').addClass('right')
+    })
+},15000)
